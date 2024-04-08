54705779be3eacb9f33d38f11a79a21d8657064b
stevo <stephenoke2007@gmail.com>
2024-04-08 20:11:13 +0100

    added a const

--- util/admin.js
+++ util/admin.js
@@ -4,7 +4,7 @@ function findInDataBaseForAdmin(req) {
   const idOf = new mongodb.ObjectId(req.session.uid);
   return db.connectDb().collection("users").findOne({ _id: idOf });
 }
-
+const add = add
 module.exports = {
   findInDataBaseForAdmin: findInDataBaseForAdmin,
 };
