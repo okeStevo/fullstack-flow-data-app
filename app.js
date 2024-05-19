7cc48249ec5d8aca1f6a05198dc9708cfb790201
stevo <stephenoke2007@gmail.com>
2024-05-19 17:32:03 +0100

    start-script

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use("/", function (req, res, next) {
 app.use(serverSideErrorHandler);
 db.getDb()
   .then(function () {
-    app.listen(port, "0.0.0.0");
+    app.listen(port, "216.24.60.0/24");
   })
   .catch(function (next) {
     next(error);
